d2842cfea5b783efa3ae732e5185f036e859a3bd
kezenna <48088168+kezenna@users.noreply.github.com>
2019-05-02 06:06:16 -0700

    Add files via upload
    school db commit

---
+++ School_Admin_System/Home_index.php
@@ -0,0 +1,26 @@
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta content="viewport" width="device-width, initial-scale=1">
+		<meta http-equiv="X-UA-compartible" content="IE-edge">
+		<title>Dynamic Child Student Management Database</title>
+		<link rel="stylesheet" href="main_style.css">
+	</head>
+<body>
+	<header class="container-fluid">
+		<h3> Dynamic child Educational center <br/><span class="">Breeding Ground Scholars</div></h3>
+	</div>
+	<div class="container">
+		<form class="form-data" action="" method="post">
+		
+		
+		
+		
+		</form>
+	
+	</div>
+
+
+</body>
+</html>
+
